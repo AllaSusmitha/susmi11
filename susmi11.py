95e5d7eacdc0e4c4ae1cbc51bc5bf11a3af4d50b
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-01 15:01:27 +0530

    Update susmi11.py

--- susmi11.py
+++ susmi11.py
@@ -1,9 +1,9 @@
 x=float(input())
 y=float(input())
 z=float(input())
-if (x > y) and (x > z):
+if (x >= y) and (x >= z):
 	largest =x
-elif (y > x) and (y > z):
+elif (y >= x) and (y >= z):
 	largest = y
 else:
 	largest = z
